08332c2e80a86951ce954d7deb16481e78dc1991
John Daly <jpdaly@gmail.com>
2017-01-02 21:54:59 +0000

    add variables

---
+++ orangetree.rb
@@ -0,0 +1,18 @@
+class OrangeTree
+  def initialize()
+    @height = 0
+    @age = 0
+    @orange_count = 0
+
+  end
+
+  def height
+    return height
+  end
+
+  def one_year_passes
+    @age += 1
+    @height += 5
+    @age > 3 ? @orange_count += 10 : orange_count += 0
+  end
+
