d2a74daabb402c04c5e090f1b7ec849c3c306eae
John Daly <jpdaly@gmail.com>
2017-01-02 20:00:53 +0000

    added quick sort method

--- arraybuilder.rb
+++ arraybuilder.rb
@@ -1,11 +1,31 @@
 #Things to try p. 56
+#still to-do
+#===========
+#reject non-alpha characters
 
-print "Please enter a word to add to the array:"
-new_word = gets.chomp
-word_list = []
+def quicksort(array)
 
-until new_word == ''
-  word_list << new_word
+  # method nicked off of Pine, p. 144
+  # could this be an Array method?
+
+  return array if array.length <= 1
+  middle = array.pop
+  less = array.select { |x| x.downcase < middle.downcase }
+  more = array.select { |x| x.downcase >= middle.downcase }
+
+  quicksort(less) + [middle] + quicksort(more)
 end
 
-print word_list.sort
+
+word_list = []
+
+print "Please enter a word to add to the array: "
+new_word = gets.chomp
+
+while new_word != ''
+  word_list << new_word
+  print "Add another word, or press <return> to exit: "
+  new_word = gets.chomp
+end
+
+print quicksort(word_list)
